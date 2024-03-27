9b0dce3f4056be368f96a9ec32428de431e0417d
WasadePranav <155419954+WasadePranav@users.noreply.github.com>
2024-03-27 13:53:02 +0530

    Delete Jwt token directory

--- Jwt token/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-
-const express = require ("express") ;
-const router = express.Router() ;
-const {singUpValidation} = require ('../helper/validation')
-const  userController = require ('../controller/userController')
-
-router.post ('/register' ,singUpValidation,userController.register)
-
-module.exports = router ;
